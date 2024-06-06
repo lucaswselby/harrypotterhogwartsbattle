5172b893cc1c300cb582301fee88a91760f7d242
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2024-06-05 17:20:24 -0700

    characters and proficiencies

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
+// display player proficiecy choice
+const displayProficiencyChoice = playerNumber => {
+    if (document.getElementById(`player${playerNumber}Character`).value) {
+        document.getElementById(`player${playerNumber}Proficiency`).style.display = "initial";
+    }
+    else {
+        document.getElementById(`player${playerNumber}Proficiency`).style.display = "none";
+    }
+}
+document.getElementById("player3Character").onchange = () => {
+    displayProficiencyChoice(3);
+}
+document.getElementById("player4Character").onchange = () => {
+    displayProficiencyChoice(4);
+}
+
+// display game
 //document.getElementById("submitPlayers").onclick = () => {
     document.getElementsByTagName("MAIN")[0].style.display = "flex";
 
     class Player {
-        constructor(character) {
+        constructor(character, proficiency) {
             this._character = character;
+            this._characterImage = `<img id="playerCharacter" src="./images/${character[0].toLowerCase() + character.replaceAll(" ", "").substring(1)}.png" alt="${character}">`;
+            this._proficiency = proficiency;
+            this._proficiencyImage = `<img id="playerCharacter" src="./images/${proficiency[0].toLowerCase() + proficiency.replaceAll(" ", "").substring(1)}.png" alt="${proficiency}">`
             this._health = 10;
         }
         get character() {
             return this._character;
+        }
+        get characterImage() {
+            return this._characterImage;
+        }
+        get proficiency() {
+            return this._proficiency;
+        }
+        get proficiencyImage() {
+            return this._proficiencyImage;
         }
         get health() {
             return this._health;
@@ -22,16 +51,25 @@
             }
         }
     }
-    const player1 = new Player(document.getElementById("player1Character").value);
-    const player2 = new Player(document.getElementById("player2Character").value);
-    let activePlayer = player1;
+    const player1 = new Player(document.getElementById("player1Character").value, document.getElementById("player1Proficiency").value);
+    const player2 = new Player(document.getElementById("player2Character").value, document.getElementById("player2Proficiency").value);
+    const players = [player1, player2];
+    if (document.getElementById("player3Character").value) players.push(new Player(document.getElementById("player3Character").value, document.getElementById("player3Proficiency").value));
+    if (document.getElementById("player4Character").value) players.push(new Player(document.getElementById("player4Character").value, document.getElementById("player4Proficiency").value));
+    let activePlayer = players[0];
 
     document.getElementsByTagName("MAIN")[0].innerHTML = `<div id="gameBoardContainer">
         <img id="gameBoard" src="./images/board.png" alt="game board">
     </div>
-    <div id="playerBoardContainer">
-        <img id="playerBoard" src="./images/playerBoard.png" alt="player board">
-        <img id="healthTracker" src="./images/healthTracker.png" alt="health tracker">
+    <div id=playerContainer>
+        <div style="display: flex">
+            <!-- ${activePlayer.characterImage} -->
+            <!-- ${activePlayer.proficiencyImage} -->
+        </div>
+        <div id="playerBoardContainer">
+            <img id="playerBoard" src="./images/playerBoard.png" alt="player board">
+            <img id="healthTracker" src="./images/healthTracker.png" alt="health tracker">
+        </div>
     </div>`;
 
     const loseHealth = health => {
