be55bb6ebc7b726537a57d16cf3bf00bd295d1a8
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2024-06-25 17:49:32 -0700

    forced discard removes from passives

--- main.js
+++ main.js
@@ -455,6 +455,7 @@ document.getElementById("submitPlayers").onclick = () => {
                     this.health--;
                 }
 
+                this._passives.splice(this.passives.indexOf(this.hand[i]), 1);
                 this.discardAt(index);
             }
             populateHand() {
