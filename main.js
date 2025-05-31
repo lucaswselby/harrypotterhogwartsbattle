65cb4b21fdcf92354e55b19cc9f5d3acb6b22b01
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2025-05-31 13:29:16 -0700

    fluffy fix

--- main.js
+++ main.js
@@ -2007,20 +2007,10 @@ document.getElementById("submitPlayers").onclick = () => {
                             };
                         });
                     }
-                };
-                if (activePlayer.health) {
-                    addPlayerChoice("Lose:", () => {items = items.filter(card => {return activePlayer.hand.includes(card);}); if (items.length) return 2; return 0;}, 1, () => {
-                        document.getElementsByClassName("choice")[0].innerHTML = `<div class="choiceContainer">${healthToken}</div>`; 
-                        document.getElementsByClassName("choice")[0].onclick = () => {activePlayer.health--; items.pop(); fluffyEffect();}; 
-                        document.getElementsByClassName("choice")[1].innerHTML = choiceScroll(activePlayer.hand); 
-                        document.getElementsByClassName("choice")[1].onclick = () => {
-                            addPlayerChoice("Discard:", () => {return activePlayer.hand.length;}, 1, fluffyDiscard);
-                        };
-                    });
-                }
-                else fluffyDiscard();
-            }; 
-            fluffyEffect();
+                    else fluffyDiscard();
+                }; 
+                fluffyEffect();
+            }
         }, () => {players.forEach(player => {player.health++; player.drawCards(1);});});
         const norbert = new Villain("Norbert", "Box 1", "creature", 0, 6, () => {activePlayer.health -= 1 + activePlayer.hand.filter(card => {return card.name === "Detention";}).length;}, () => {players.forEach(player => {
             addPlayerChoice(`Banish for ${player.hero}:`, () => {return 2;}, 1, () => {
