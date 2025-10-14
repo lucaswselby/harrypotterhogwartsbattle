af973a8a46ded410555c6caec31fa52efa47e0e9
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2025-10-13 17:18:37 -0700

    confundus fix

--- main.js
+++ main.js
@@ -2362,7 +2362,7 @@ document.getElementById("submitPlayers").onclick = () => {
                     else this.attackDamageTaken++;
 
                     // Confundus effect
-                    if (activeVillains.filter(villain => {return villain.type.includes("villain") && villain.attackDamageTaken;}).length && (activePlayer.passives.includes(confundus1) || activePlayer.passives.includes(confundus2))) {
+                    if (activeVillains.filter(villain => {return villain.type.includes("villain") && villain.attackDamageTaken;}).length === activeVillains.filter(villain => {return villain.type.includes("villain");}).length && (activePlayer.passives.includes(confundus1) || activePlayer.passives.includes(confundus2))) {
                         if (activePlayer.passives.includes(confundus1)) {
                             activePlayer.passives.splice(activePlayer.passives.indexOf(confundus1), 1);
                             activeLocation.removeFromLocation();
